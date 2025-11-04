420fc7f8aedfecfc32ff915c8ab09a5c89e23533
2paperstar <lee@paperst.ar>
2025-11-04 20:38:56 +0900

    chore: add public key

--- lib/app/values/config.dart
+++ lib/app/values/config.dart
@@ -19,4 +19,7 @@ abstract class Config {
   static const String playStoreUrl = _Config.playStoreUrl;
   @EnviedField(defaultValue: 'https://apps.apple.com/app/id6744280856')
   static const String appStoreUrl = _Config.appStoreUrl;
+
+  @EnviedField()
+  static const String apiChannelKeyPublicKey = _Config.apiChannelKeyPublicKey;
 }
